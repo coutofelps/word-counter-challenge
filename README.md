3464b9873d3413d0ae7804ac83f5205acdcb0a3a
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-04-13 17:56:12 -0300

    :sparkles: initial commit

--- README.md
+++ README.md
@@ -1,70 +1,40 @@
-# Getting Started with Create React App
+# Word Counter Challenge
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Short description or a sentence describing the purpose of the project.
 
-## Available Scripts
+## Prerequisites
 
-In the project directory, you can run:
+List any software or libraries required to run the project, such as Node.js, npm, etc.
 
-### `npm start`
+## Installation
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+1. Clone this repository.
+2. Navigate to the project directory in the terminal.
+3. Run the command `npm install` to install the dependencies.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Usage
 
-### `npm test`
+Explain how to start and use the project. For example:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+```
+    npm start
+```
 
-### `npm run build`
+This will start the development server, and you can view the project in the browser at `http://localhost:3000`.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Contribution
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Explain how others can contribute to your project. This may include instructions on how to:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+- Open issues to report bugs or request new features.
+- Create pull requests to propose changes to the code.
 
-### `npm run eject`
+## License
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+Specify the license under which the project is being made available. For example:
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+## Contact
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Provide contact information for the project developers, such as email or links to social media.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
+import { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Application() {
+    const [currentStep, setCurrentStep] = useState(1)
+    const [textValue, setTextValue] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+    const [wordCount, setWordCount] = useState(0)
+
+    useEffect(() => {
+        document.title = 'Word Counter Challenge'
+      }, [])
+
+    const handleCountButtonClick = (event) => {
+        event.preventDefault()
+
+        if (textValue.trim() === '') {
+            setErrorMessage('Please enter at least one word.')
+        } else {
+            setErrorMessage('')
+            setCurrentStep(2)
+            countWords(textValue)
+        }
+    }
+
+    const countWords = (text) => {
+        const words = text.trim().split(/\s+/) // Split the text into words using whitespace as delimiters
+
+        setWordCount(words.length)
+    }
+
+    const handleResetButtonClick = () => {
+        setCurrentStep(1)
+        setTextValue('')
+        setErrorMessage('')
+        setWordCount(0)
+    }
+
+    return (
+        <>
+            <div className='formbold-main-wrapper'>
+                <div className='formbold-form-wrapper'>
+                    <form>
+                        <div className='formbold-steps'>
+                            <ul>
+                                <li className={`formbold-step-menu1 ${currentStep === 1 ? 'active' : ''}`}>
+                                    <span>1</span>
+                                    Typing text
+                                </li>
+                                <li className={`formbold-step-menu2 ${currentStep === 2 ? 'active' : ''}`}>
+                                    <span>2</span>
+                                    Counting words
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className={`formbold-form-step-1 ${currentStep === 1 ? 'active' : ''}`}>
+                            <div>
+                                <label className='formbold-form-label'>Type your text</label>
+                                <textarea rows={6} name='text' id='text' value={textValue} onChange={(e) => setTextValue(e.target.value)} placeholder='Type your text here' className='formbold-form-input'></textarea>
+                            </div>
+
+                            {errorMessage && <p className='error-message'>{errorMessage}</p>}
+                        </div>
+
+                        <div className={`formbold-form-step-2 ${currentStep === 2 ? 'active' : ''}`}>
+                            <div>
+                                <label className='success-message'>Your text has {wordCount} words.</label>
+                            </div>
+                        </div>
+
+                        <div className='formbold-form-btn-wrapper'>
+                            {currentStep === 1 && (
+                                <button id='countButton' className='formbold-btn' onClick={handleCountButtonClick}>
+                                    Count
+                                    <svg width='16' height='16' viewBox='0 0 16 16' fill='none'>
+                                        <g clip-path='url(#clip0_1675_1807)'>
+                                            <path d='M10.7814 7.33312L7.20541 3.75712L8.14808 2.81445L13.3334 7.99979L8.14808 13.1851L7.20541 12.2425L10.7814 8.66645H2.66675V7.33312H10.7814Z' fill='white'></path>
+                                        </g>
+                                        <defs>
+                                            <clipPath id='clip0_1675_1807'>
+                                                <rect width='16' height='16' fill='white'></rect>
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                </button>
+                            )}
+                            {currentStep === 2 && (
+                                <button className='formbold-btn' onClick={handleResetButtonClick}>
+                                    Reset
+                                </button>
+                            )}
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
-
-export default App;
